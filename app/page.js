60ce3f1b42329eecb7c2a1f1df9533357a669f9e
Rosie Chen <Katekyo.069@gmail.com>
2025-03-22 14:36:48 +0800

    save

--- app/page.js
+++ app/page.js
@@ -4,12 +4,17 @@ import ChatRoom from "@/components/ChatRoom";
 import Login from "@/components/Login";
 import Sidebar from "@/components/Sidebar";
 import Option from "@/components/Option";
-import { useRef, useState } from "react";
+import { useRef, useState, Suspense } from "react";
 import { io } from "socket.io-client";
 import { Toaster } from 'react-hot-toast';
+import Script from 'next/script';
 
 const socket = io("https://server-chat-app-iu9t.onrender.com");
 // const socket = io("http://localhost:3001");
+
+const LazyChatRoom = React.lazy(() => import('@/components/ChatRoom'));
+const LazySidebar = React.lazy(() => import('@/components/Sidebar'));
+const LazyOption = React.lazy(() => import('@/components/Option'));
 
 export default function Home() {
 
@@ -35,6 +40,10 @@ export default function Home() {
 
   return (
     <div className="h-[100vh]">
+      <Script strategy="afterInteractive" // 在頁面渲染完成後加載
+              src="https://example.com/external-script.js"
+      />
+
       <Toaster />
       {!isLogin?
         <Login 
@@ -49,61 +58,63 @@ export default function Home() {
             setMessages={setMessages}
             setUserImage={setUserImage}/>:
         <div className="w-full flex flex-row h-[100vh]">
-          <div className={`${isOpenMessageRoom ? "hidden md:flex" : "w-full"} md:w-[30%] h-full flex flex-col md:flex-row`}>
-            {/* Option 固定高度 */}
-            <div className="md:flex md:w-1/6 md:border-r-[1px] justify-center order-last md:order-first flex-shrink-0">
-              <Option 
-                userInfo={userInfo} 
-                setIsLogin={setIsLogin} 
-                swithTo={swithTo} 
-                setSwitchTo={setSwitchTo} 
-                setReceiver={setReceiver}
-                setIsProfileEdit={setIsProfileEdit}
-                userImage={userImage}
-                setGroupName={setGroupName}
-              />
-            </div>
+          <Suspense fallback={<div>Loading...</div>}>
+            <div className={`${isOpenMessageRoom ? "hidden md:flex" : "w-full"} md:w-[30%] h-full flex flex-col md:flex-row`}>
+              {/* Option 固定高度 */}
+              <div className="md:flex md:w-1/6 md:border-r-[1px] justify-center order-last md:order-first flex-shrink-0">
+                <LazyOption 
+                  userInfo={userInfo} 
+                  setIsLogin={setIsLogin} 
+                  swithTo={swithTo} 
+                  setSwitchTo={setSwitchTo} 
+                  setReceiver={setReceiver}
+                  setIsProfileEdit={setIsProfileEdit}
+                  userImage={userImage}
+                  setGroupName={setGroupName}
+                />
+              </div>
 
-            {/* Sidebar 高度等於 Option 剩餘的高度 */}
-            <div className="w-full h-full md:h-auto md:w-5/6 pb-4 flex-1 justify-center items-center flex-col" style={{height: 'calc(100vh - 60px)'}}>
-              <Sidebar 
-                groupName={groupName}
-                setGroupName={setGroupName}
-                userName={userName} 
-                userInfo={userInfo} 
-                setReceiver={setReceiver} 
-                receiver={receiver} 
-                friendInfo={friendInfo} 
-                setFriendInfo={setFriendInfo} 
-                swithTo={swithTo} 
+              {/* Sidebar 高度等於 Option 剩餘的高度 */}
+              <div className="w-full h-full md:h-auto md:w-5/6 pb-4 flex-1 justify-center items-center flex-col" style={{height: 'calc(100vh - 60px)'}}>
+                <LazySidebar 
+                  groupName={groupName}
+                  setGroupName={setGroupName}
+                  userName={userName} 
+                  userInfo={userInfo} 
+                  setReceiver={setReceiver} 
+                  receiver={receiver} 
+                  friendInfo={friendInfo} 
+                  setFriendInfo={setFriendInfo} 
+                  swithTo={swithTo} 
+                  messages={messages}
+                  groupMember={groupMember} 
+                  setGroupMember={setGroupMember}
+                  socket={socket}
+                  setSwitchTo={setSwitchTo}
+                  isProfileEdit={isProfileEdit} 
+                  setIsProfileEdit={setIsProfileEdit}
+                  setUserImage={setUserImage}
+                  setMessages={setMessages}
+                  setIsOpenMessageRoom={setIsOpenMessageRoom}
+                />
+              </div>
+            </div>
+            <div className={`${isOpenMessageRoom? 'w-full':'hidden'} md:flex md:w-[70%] flex-col h-[100vh]`}>
+              <LazyChatRoom
                 messages={messages}
-                groupMember={groupMember} 
-                setGroupMember={setGroupMember}
-                socket={socket}
-                setSwitchTo={setSwitchTo}
-                isProfileEdit={isProfileEdit} 
-                setIsProfileEdit={setIsProfileEdit}
-                setUserImage={setUserImage}
                 setMessages={setMessages}
+                socket={socket}
+                user={userMail}
+                receiver={receiver}
+                userInfo={userInfo}
+                friendInfo={friendInfo}
+                groupMember={groupMember} 
+                setFriendInfo={setFriendInfo}
+                setReceiver={setReceiver}
                 setIsOpenMessageRoom={setIsOpenMessageRoom}
               />
             </div>
-          </div>
-          <div className={`${isOpenMessageRoom? 'w-full':'hidden'} md:flex md:w-[70%] flex-col h-[100vh]`}>
-            <ChatRoom
-              messages={messages}
-              setMessages={setMessages}
-              socket={socket}
-              user={userMail}
-              receiver={receiver}
-              userInfo={userInfo}
-              friendInfo={friendInfo}
-              groupMember={groupMember} 
-              setFriendInfo={setFriendInfo}
-              setReceiver={setReceiver}
-              setIsOpenMessageRoom={setIsOpenMessageRoom}
-            />
-          </div>
+          </Suspense >
         </div>
       }
     </div>
